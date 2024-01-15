90fcf4016db575d67e88f7b4c578401356fa6679
junghoon0904 <153577771+junghoon0904@users.noreply.github.com>
2024-01-15 16:23:24 +0900

    JHLee commit complete

---
+++ js/movie_array.js
@@ -0,0 +1,11 @@
+var movieArray = [
+    [1, "images/kor_1.jpg", "괴물", "송강오 등등", "괴물이 나오는 영화", "https://www.youtube.com/embed/8DtrRadXsjI?autoplay=1&mute=1"],
+    [2, "images/kor_2.jpg", "도둑들", "김혜수 등등", "도둑영화",],
+    [3, "images/kor_3.jpg", "7번방의 선물", "등등", "감빵영화",],
+    [1, "images/kor_4.jpg", "괴물", "송강오 등등", "괴물이 나오는 영화",],
+    [2, "images/kor_5.jpg", "도둑들", "김혜수 등등", "도둑영화",],
+    [3, "images/kor_6.jpg", "7번방의 선물", "등등", "감빵영화",],
+    [1, "images/kor_7.jpg", "괴물", "송강오 등등", "괴물이 나오는 영화",],
+    [2, "images/kor_8.jpg", "도둑들", "김혜수 등등", "도둑영화",],
+    [3, "images/kor_9.jpg", "7번방의 선물", "등등", "감빵영화",],
+];
